8445db84e404eba87e7f564d854c242aa67e9eaf
Cullen <cullenyanderson@gmail.com>
2021-01-11 17:29:04 -0500

    new frontend

--- machinelearning/redditapi.py
+++ machinelearning/redditapi.py
@@ -13,39 +13,9 @@ def get_subreddit(reddit, subreddit):
     return subreddit
 
 
-# def get_subreddit(reddit):
-#     while True:
-#         u_sub = input('Type a subreddit to analyze: ')
-#         try:
-#             subreddit = reddit.subreddits.search_by_name(u_sub, exact=True)[0]
-#             print()
-#             return subreddit
-#         except NotFound:
-#             print("Subreddit does not exist! Please try again.")
-#             continue
-#         except ResponseException:
-#             print("Could not access subreddit! Please try again")
-#             continue
-
-
 def get_posts(subreddit):
     posts = set()  # use a set to clear any duplicates
     for post in subreddit.new(limit=50):
         posts.add(post)
     posts = list(posts)  # easier to work with lists
     return posts
-
-
-# def get_posts(subreddit):
-#     try:
-#         posts = set()  # use a set to clear any duplicates
-#         for post in subreddit.new(limit=50):
-#             posts.add(post)
-#         posts = list(posts)  # easier to work with lists
-#         return posts
-#     except NotFound:
-#         print("Subreddit does not exist! Please try again.")
-#         sys.exit(1)
-#     except Forbidden:
-#         print("Could not access subreddit! Quitting program.")
-#         sys.exit(1)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
-from machinelearning.main import sentiment_analysis
-from prawcore.exceptions import NotFound, Forbidden
+from machinelearning.mainml import sentiment_analysis
+from prawcore.exceptions import NotFound, Forbidden, ResponseException
 
 app = Flask(__name__)
 
@@ -11,12 +11,15 @@ def index():
         subreddit = request.form["subreddit"]
         try:
             most_positive, most_negative, percentages = sentiment_analysis(subreddit)
-        except NotFound or Forbidden:
-            error = True
-            return render_template('thing.html', error=error)
-        return render_template('thing.html',
+        except NotFound or Forbidden or ResponseException:
+            error = "Subreddit does not exist. Please try again."
+            return render_template('main.html', error=error)
+        except IndexError:
+            error = "Please enter a subreddit to analyze."
+            return render_template('main.html', error=error)
+        return render_template('main.html',
                                positive=most_positive,
                                negative=most_negative,
                                percentages=percentages)
     else:
-        return render_template('thing.html')
+        return render_template('main.html')
